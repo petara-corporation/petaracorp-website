599c881762e0b73bf3f49f0f75a656bfeada633e
Kaustubh Vyas <kaustubhvyas34@gmail.com>
2024-01-10 00:48:59 +0530

    window definition fix

--- src/app/components/hero-slider/hero-slider.js
+++ src/app/components/hero-slider/hero-slider.js
@@ -4,34 +4,12 @@ import { Carousel } from 'react-responsive-carousel';
 import styles from './hero-slider.module.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const HeroCarousel = () => (
-  <div className='relative w-full mb-10'>
-    {window.innerWidth < 768 ? (
-      <div className={styles.mobileView}>
-        <img alt='Premium Inflatable Solutions' src='./slide1.jpg' />
-        <div className={`${styles.overlayText} legend`}>
-          <div className={styles.textWrap}>
-            <h1>We’re on a mission to craft lasting impressions!</h1>
-            <p>
-              Unleash a Petara i.e. "a Basket" of solutions that are guaranteed
-              to amaze your clients - from Inflatables to personalized gifting
-              solutions, we craft everything that catches the eye and soul.
-            </p>
-            <a href='/products'>
-              <button>Explore</button>
-            </a>
-          </div>
-        </div>
-      </div>
-    ) : (
-      <Carousel
-        autoPlay
-        infiniteLoop
-        showStatus={false}
-        showThumbs={false}
-        dynamicHeight={true}
-      >
-        <div className='slide'>
+const HeroCarousel = () => {
+  var window = global.window || {};
+  return (
+    <div className='relative w-full mb-10'>
+      {window.innerWidth < 768 ? (
+        <div className={styles.mobileView}>
           <img alt='Premium Inflatable Solutions' src='./slide1.jpg' />
           <div className={`${styles.overlayText} legend`}>
             <div className={styles.textWrap}>
@@ -48,19 +26,45 @@ const HeroCarousel = () => (
             </div>
           </div>
         </div>
-        {/* <div className="slide">
-        <img alt="Premium Inflatable Solutions" src="./slide2.jpg" />
-        <div className={`${styles.overlayText} legend`}>
-          <div className={styles.textWrap}>
-            <h1>We’re on a mission to craft lasting impressions!</h1>
-            <p>Unleash a Petara (basket) of solutions that are guaranteed to amaze your clients - from Inflatables to personalized gifting solutions, we craft everything that catches the eye and soul.</p>
-            <button>Explore</button>
+      ) : (
+        <Carousel
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          showThumbs={false}
+          dynamicHeight={true}
+        >
+          <div className='slide'>
+            <img alt='Premium Inflatable Solutions' src='./slide1.jpg' />
+            <div className={`${styles.overlayText} legend`}>
+              <div className={styles.textWrap}>
+                <h1>We’re on a mission to craft lasting impressions!</h1>
+                <p>
+                  Unleash a Petara i.e. "a Basket" of solutions that are
+                  guaranteed to amaze your clients - from Inflatables to
+                  personalized gifting solutions, we craft everything that
+                  catches the eye and soul.
+                </p>
+                <a href='/products'>
+                  <button>Explore</button>
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div> */}
-      </Carousel>
-    )}
-  </div>
-);
+          {/* <div className="slide">
+          <img alt="Premium Inflatable Solutions" src="./slide2.jpg" />
+          <div className={`${styles.overlayText} legend`}>
+            <div className={styles.textWrap}>
+              <h1>We’re on a mission to craft lasting impressions!</h1>
+              <p>Unleash a Petara (basket) of solutions that are guaranteed to amaze your clients - from Inflatables to personalized gifting solutions, we craft everything that catches the eye and soul.</p>
+              <button>Explore</button>
+            </div>
+          </div>
+        </div> */}
+        </Carousel>
+      )}
+    </div>
+  );
+};
 
 export default HeroCarousel;
--- src/app/promotion-gifting/components/hero-slider-corporate/hero-slider-corporate.js
+++ src/app/promotion-gifting/components/hero-slider-corporate/hero-slider-corporate.js
@@ -4,28 +4,12 @@ import { Carousel } from 'react-responsive-carousel';
 import styles from './hero-slider.module.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const HeroCarouselCorporate = () => (
-  <div className='relative w-full mb-10'>
-    {window.innerWidth < 768 ? (
-      <div className={styles.mobileView}>
-        <img alt='Premium Corporate Gifting Solutions' src='./slide-3.jpg' />
-        <div className={`${styles.overlayText} legend`}>
-          <div className={styles.textWrap}>
-            <h1>Premium Corporate Gifting</h1>
-            <p>Your allies in creating a memorable brand</p>
-            <button>Explore</button>
-          </div>
-        </div>
-      </div>
-    ) : (
-      <Carousel
-        autoPlay
-        infiniteLoop
-        showStatus={false}
-        showThumbs={false}
-        dynamicHeight={true}
-      >
-        <div className='slide'>
+const HeroCarouselCorporate = () => {
+  var window = global.window || {};
+  return (
+    <div className='relative w-full mb-10'>
+      {window.innerWidth < 768 ? (
+        <div className={styles.mobileView}>
           <img alt='Premium Corporate Gifting Solutions' src='./slide-3.jpg' />
           <div className={`${styles.overlayText} legend`}>
             <div className={styles.textWrap}>
@@ -35,19 +19,41 @@ const HeroCarouselCorporate = () => (
             </div>
           </div>
         </div>
-        {/* <div className="slide">
-        <img alt="Premium Inflatable Solutions" src="./slide-4.jpg" />
-        <div className={`${styles.overlayText} legend`}>
-          <div className={styles.textWrap}>
-            <h1>Customize Gift Packing</h1>
-            <p>for Your Business Success</p>
-            <button>Explore</button>
+      ) : (
+        <Carousel
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          showThumbs={false}
+          dynamicHeight={true}
+        >
+          <div className='slide'>
+            <img
+              alt='Premium Corporate Gifting Solutions'
+              src='./slide-3.jpg'
+            />
+            <div className={`${styles.overlayText} legend`}>
+              <div className={styles.textWrap}>
+                <h1>Premium Corporate Gifting</h1>
+                <p>Your allies in creating a memorable brand</p>
+                <button>Explore</button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div> */}
-      </Carousel>
-    )}
-  </div>
-);
+          {/* <div className="slide">
+          <img alt="Premium Inflatable Solutions" src="./slide-4.jpg" />
+          <div className={`${styles.overlayText} legend`}>
+            <div className={styles.textWrap}>
+              <h1>Customize Gift Packing</h1>
+              <p>for Your Business Success</p>
+              <button>Explore</button>
+            </div>
+          </div>
+        </div> */}
+        </Carousel>
+      )}
+    </div>
+  );
+};
 
 export default HeroCarouselCorporate;
