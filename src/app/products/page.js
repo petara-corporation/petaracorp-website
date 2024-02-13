ef0508de89fca0b00a8d11ce08f09b3e6529a519
Kaustubh Vyas <kaustubhvyas34@gmail.com>
2024-02-13 23:58:17 +0530

    hyperlink fixes and text updates

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -13,6 +13,8 @@ export default function Products() {
         'Ground Inflatable Product Shape ',
         'Ground Inflatable Character Shape',
       ],
+      description:
+        'Elevate events with attention-grabbing, larger-than-life inflatables',
     },
     {
       id: 2,
@@ -31,6 +33,8 @@ export default function Products() {
         'Fun Interactive : Interactive Bouncy',
         'Fun Tunnel  : Tunnel Bouncy',
       ],
+      description:
+        'Foster joy and physical development with our vibrant bouncy products',
     },
     {
       id: 3,
@@ -42,6 +46,8 @@ export default function Products() {
         'Inflatable Costume : Character Shape ',
         'Fur Costume',
       ],
+      description:
+        'Transform moments with playful, interactive and creative inflatable costumes',
     },
     {
       id: 4,
@@ -49,6 +55,8 @@ export default function Products() {
       img: '/inflatable_archs_kiosks.png',
       href: '/products/archs-&-kiosk',
       subProducts: ['Arches & Kiosk', 'Balloons & Blimps'],
+      description:
+        'Make a grand entrance or create unique event spaces effortlessly',
     },
     {
       id: 5,
@@ -56,6 +64,8 @@ export default function Products() {
       img: '/Balloon2.png',
       href: '/products/balloons-blimps',
       subProducts: ['Arches & Kiosk', 'Balloons & Blimps'],
+      description:
+        'Store Openings, Product Launches, Parades, Outdoor Events, Marketing Campaigns',
     },
   ];
   return (
@@ -82,26 +92,27 @@ export default function Products() {
       <div className={styles.products}>
         <div className={`${styles.subnav} container mx-auto`}>
           <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
-            {productList.map(sub => {
+            {productList.map(product => {
               return (
-                <li className={`${styles.productItem}`} key={sub?.id}>
-                  <a href={sub?.href} className='block h-full'>
+                <li className={`${styles.productItem}`} key={product?.id}>
+                  <a href={product?.href} className='block h-full'>
                     <div className={`${styles.layerWrap} relative`}>
                       <div className={styles.layerImageWrap}>
                         <Image
                           className={`${styles.gridImg} relative`}
-                          src={sub.img}
-                          alt={sub.label}
+                          src={product.img}
+                          alt={product.label}
                           fill={true}
                           sizes='(max-width: 768px) 25vw, 25vw'
                         />
                         <div className={styles.overlay}>
-                          <h2>{sub.label}</h2>
+                          <h2>{product.label}</h2>
                         </div>
                       </div>
                       <div className={styles.hoverLayer}>
                         <div className={styles.text}>
-                          <h2>{sub.label}</h2>
+                          <h2>{product.label}</h2>
+                          <h3>{product.description}</h3>
                         </div>
                       </div>
                     </div>
--- src/app/promotion-gifting/components/products/product-listing.js
+++ src/app/promotion-gifting/components/products/product-listing.js
@@ -7,16 +7,19 @@ export default function CorporateProductListing() {
       id: 1,
       name: 'Corporate Gifting',
       url: '/images/promotions-giftings/corporate-gifts/tile-corporate.jpg',
+      href: '/promotion-gifting/corporate-gifting',
     },
     {
       id: 2,
       name: 'Personal Utility Gifting',
       url: '/images/promotions-giftings/personal-utilities/Tile-personal.jpg',
+      href: '/promotion-gifting/personal-utility-gifting',
     },
     {
       id: 3,
       name: 'Product Promotion',
       url: '/images/promotions-giftings/promotional-products/Tile-product.jpg',
+      href: '/promotion-gifting/product-promotion',
     },
   ];
   return (
@@ -33,16 +36,18 @@ export default function CorporateProductListing() {
         >
           {productList.map(product => {
             return (
-              <li key={product.id} className={`${styles.prodCard} relative`}>
-                <Image
-                  className={`${styles.gridImgPortfolio} relative`}
-                  src={product.url}
-                  alt={product.name}
-                  fill={true}
-                  sizes='(max-width: 768px) 25vw, 25vw'
-                />
-                <p className='text-center mt-4'>{product.name}</p>
-              </li>
+              <a href={product?.href} className='block h-full'>
+                <li key={product.id} className={`${styles.prodCard} relative`}>
+                  <Image
+                    className={`${styles.gridImgPortfolio} relative`}
+                    src={product.url}
+                    alt={product.name}
+                    fill={true}
+                    sizes='(max-width: 768px) 25vw, 25vw'
+                  />
+                  <p className='text-center mt-4'>{product.name}</p>
+                </li>
+              </a>
             );
           })}
         </ul>
