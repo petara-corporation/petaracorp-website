742300386b9a3f90f595d3df15aa1eb20b4f457c
Kaustubh Vyas <kaustubhvyas34@gmail.com>
2024-10-01 11:52:36 +0530

    JSON parsing fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,68 +3,71 @@ import './globals.css';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
 
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Petara Corporation",
+  "url": "https://www.petaracorp.com/",
+  "description": "Inflatables, Bouncy & Corporate Gifting Provider. Petara Corporation offers premium quality products for all events.",
+  "logo": "https://www.petaracorp.com/images/petara_logo_landscape.jpg",
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+91 88792 92103",
+    "contactType": "Customer Service",
+    "email": "sales@petaracorp.com",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "I - 5, Clarion Park, Aundh",
+      "addressLocality": "Pune",
+      "postalCode": "411007",
+      "addressCountry": "IN"
+    }
+  },
+  "sameAs": [
+    "https://www.facebook.com/share/hVc4mo2gMNxvbCFi/",
+    "https://www.instagram.com/petaracorp/",
+    "https://www.linkedin.com/company/petaracorp/"
+  ],
+  "breadcrumb": {
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Home",
+        "item": "https://www.petaracorp.com/"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "Product Promotion",
+        "item": "https://www.petaracorp.com/products"
+      },
+      {
+        "@type": "ListItem",
+        "position": 3,
+        "name": "Promotion & Gifting",
+        "item": "https://www.petaracorp.com/promotion-gifting"
+      },
+      {
+        "@type": "ListItem",
+        "position": 4,
+        "name": "Eco-Ethnic Gifting",
+        "item": "https://www.petaracorp.com/eco-ethnic-gifting"
+      }
+    ]
+  }
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <head>
         <meta name="msvalidate.01" content="752203A88C7F123A5D9EB3E811E1CF26" />
-        <script type="application/ld+json">
-          {`{
-            "@context": "https://schema.org",
-            "@type": "Organization",
-            "name": "Petara Corporation",
-            "url": "https://www.petaracorp.com/",
-            "description": "Inflatables, Bouncy & Corporate Gifting Provider. Petara Corporation offers premium quality products for all events.",
-            "logo": "https://www.petaracorp.com/images/petara_logo_landscape.jpg",
-            "contactPoint": {
-              "@type": "ContactPoint",
-              "telephone": "+91 88792 92103",
-              "contactType": "Customer Service",
-              "email": "sales@petaracorp.com",
-              "address": {
-                "@type": "PostalAddress",
-                "streetAddress": "I - 5, Clarion Park, Aundh",
-                "addressLocality": "Pune",
-                "postalCode": "411007",
-                "addressCountry": "IN"
-              }
-            },
-            "sameAs": [
-              "https://www.facebook.com/share/hVc4mo2gMNxvbCFi/",
-              "https://www.instagram.com/petaracorp/",
-              "https://www.linkedin.com/company/petaracorp/"
-            ],
-            "breadcrumb": {
-              "@type": "BreadcrumbList",
-              "itemListElement": [
-                {
-                  "@type": "ListItem",
-                  "position": 1,
-                  "name": "Home",
-                  "item": "https://www.petaracorp.com/"
-                },
-                {
-                  "@type": "ListItem",
-                  "position": 2,
-                  "name": "Product Promotion",
-                  "item": "https://www.petaracorp.com/products"
-                },
-                {
-                  "@type": "ListItem",
-                  "position": 3,
-                  "name": "Promotion & Gifting",
-                  "item": "https://www.petaracorp.com/promotion-gifting"
-                },
-                {
-                  "@type": "ListItem",
-                  "position": 4,
-                  "name": "Eco-Ethnic Gifting",
-                  "item": "https://www.petaracorp.com/eco-ethnic-gifting"
-                },
-              ]
-            }
-          }`}
-        </script>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
       </head>
       <body>
         <Header />
